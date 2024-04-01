ee6b14067be7c1b85020e3ab9837a39320eaa752
rafael-gama <rafaelantunesjf@gmail.com>
2024-04-01 19:32:57 -0300

    feat(flix-api): adiciona projeto inicial no github

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('genres/', GenreCreateListView.as_view(), name='genre'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-datail-view'),
+
+]
