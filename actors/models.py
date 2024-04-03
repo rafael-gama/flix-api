c28f28238165bdb9e70c5b67d497b5af94c39789
rafael-gama <rafaelantunesjf@gmail.com>
2024-04-02 21:43:08 -0300

    feat(flix-api): adiciona app actors

--- actors/models.py
+++ actors/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
+
+NATIONALITY_CHOICES = (
+    ('BRA', 'Brasil'),
+    ('USA', 'Estados Unidos'),
+)
+
 class Actor(models.Model): 
-    ...
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField(
+        max_length=100, 
+        choices=NATIONALITY_CHOICES,
+        blank=True, 
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
