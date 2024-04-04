21239801d7b39a6ff30172e6609210cf60fa31ec
rafael-gama <rafaelantunesjf@gmail.com>
2024-04-03 22:52:11 -0300

    feat(flix-api): adiciona admin e serializers para o app reviews

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from actors.views   import ActorCreateListView,  ActorRetrieveUpdateDestroyView
+from genres.views   import GenreCreateListView,  GenreRetrieveUpdateDestroyView
+from movies.views   import MovieCreateListView,  MovieRetrieveUpdateDestroyView
+from reviews.views  import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -17,4 +18,8 @@ urlpatterns = [
     
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'), 
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'), 
+    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
+
+
+class ReviewCreateListView(generics.ListCreateAPIView): 
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
