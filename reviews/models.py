3dc96a47b4a5d51e83c74c77e2e970f13f9f4556
rafael-gama <rafaelantunesjf@gmail.com>
2024-04-03 22:01:28 -0300

    feat(flix-api): adiciona app reviews

---
+++ reviews/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from movies.models import Movie
+
+class Review(models.Model): 
+    movie = models.ForeignKey(
+        Movie, 
+        on_delete=models.PROTECT,
+        related_name='reviews' 
+    )
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(0, 'Avaliação não pode ser inferior à 0 estrelas.'),
+            MaxValueValidator(5, 'Avaliação não pode ser superior à 5 estrelas.'),
+        ]
+    ) 
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self): 
+        return self.movie
